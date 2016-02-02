0b07a1001b67dbbe4996a225de48383d81348b21
HansHammel <oh.schitt@freenet.de>
2016-02-02 18:19:43 +0100

    initial

---
+++ bin/create-config.js
@@ -0,0 +1,22 @@
+var inquirer = require("inquirer");
+var path=require('path');
+var fs=require('fs');
+console.log('Please input your GitHub Username and a token to access public/private repository information (to be created on GitHub Settings > Personal access tokens)');
+var questions = [
+  {
+    type: "input",
+    name: "User",
+    message: "GitHub username"
+  }, {
+    type: "input",
+    name: "token",
+    message: "GitHub token"
+  }
+  ];
+ inquirer.prompt(questions, function( answers ) {
+	answers.gitaccess = 'git';
+	console.log( JSON.stringify(answers, null, "  "));
+	fs.writeFileSync(path.join(__dirname,'../config.json'), JSON.stringify(answers, null, "  "), function(err) {
+	  if (err) { console.log(err); process.exit(1); }
+	});
+});
