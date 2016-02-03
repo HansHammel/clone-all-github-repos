9076fd6d1c8dfab104660cc0f020ac559424c138
HansHammel <oh.schitt@freenet.de>
2016-02-03 03:31:34 +0100

    cleanup

--- bin/clone-all-github-repos.js
+++ bin/clone-all-github-repos.js
@@ -2,4 +2,5 @@
 var User = require('../index.js');
 //noinspection JSFileReferences
 var user = new User(require('../config.json')); // config.js contains {token: 'your github token for public repo access', gitaccess:'git', User:'your user name'}
-user.getRepositories(function(){}); 
+user.getRepositories(function () {
+});
--- bin/create-config.js
+++ bin/create-config.js
@@ -1,23 +1,27 @@
 #! /usr/bin/env node
 var inquirer = require("inquirer");
-var path=require('path');
-var fs=require('fs');
+var path = require('path');
+var fs = require('fs');
 console.log('Please input your GitHub Username and a token to access public/private repository information (to be created on GitHub Settings > Personal access tokens)');
 var questions = [
-  {
-    type: "input",
-    name: "User",
-    message: "GitHub username"
-  }, {
-    type: "input",
-    name: "token",
-    message: "GitHub token"
-  }
-  ];
- inquirer.prompt(questions, function( answers ) {
-	answers.gitaccess = 'https';
-	console.log( JSON.stringify(answers, null, "  "));
-	fs.writeFile(path.join(__dirname,'../config.json'), JSON.stringify(answers, null, "  "), function(err) {
-	  if (err) { console.log(err); process.exit(1); }
-	});
+    {
+        type: "input",
+        name: "User",
+        message: "GitHub username"
+    }, {
+        type: "input",
+        name: "token",
+        message: "GitHub token"
+    }
+];
+//noinspection JSUnresolvedFunction
+inquirer.prompt(questions, function (answers) {
+    answers.gitaccess = 'https';
+    console.log(JSON.stringify(answers, null, "  "));
+    fs.writeFile(path.join(__dirname, '../config.json'), JSON.stringify(answers, null, "  "), function (err) {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+    });
 });
