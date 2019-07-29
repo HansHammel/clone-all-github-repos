d91a75429b9ffdb49620a076da2e3684d57f294a
HansHammel <mail@specialsolutions.de>
2019-07-29 22:25:12 +0200

    fixed build

--- bin/create-config.js
+++ bin/create-config.js
@@ -17,41 +17,41 @@ if (fs.existsSync(conffile)) {
     console.log(colors.yellow('Note: Using exisiting config file at: ' + conffile));
     console.log(colors.yellow('Note: Use cagr or clone-all-github-repos to clone all your repos into the current folder'));
 } else {
-    if (process.env.NODE_ENV != "testing")
-    {
-    console.log(colors.yellow('Please input your GitHub Username and a token to access public/private repository information (to be created on GitHub Settings > Personal access tokens)'));
-    var questions = [
-        {
-            type: "input",
-            name: "User",
-            message: "GitHub username"
-        }, {
-            type: "input",
-            name: "token",
-            message: "GitHub token"
-        }
-    ];
-//noinspection JSUnresolvedFunction
-    inquirer.prompt(questions).then(function(answers) {
-        answers.gitaccess = 'https';
-        fs.writeFile(conffile, JSON.stringify(answers, null, "  "), function (err) {
-            if (err) {
-            	  console.log(colors.red('Cannot save config!'));
-                console.log(err);
-                process.exit(1);
+    if (process.env.NODE_ENV != "testing") {
+        console.log(colors.yellow('Please input your GitHub Username and a token to access public/private repository information (to be created on GitHub Settings > Personal access tokens)'));
+        var questions = [
+            {
+                type: "input",
+                name: "User",
+                message: "GitHub username"
+            }, {
+                type: "input",
+                name: "token",
+                message: "GitHub token"
             }
-            console.log(colors.yellow('Note: Created config file at: ' + conffile));
-            console.log(colors.yellow('Note: Use cagr or clone-all-github-repos to clone all your repos into the current folder'));
-        });
-    });
-    } else {
-        fs.writeFile(conffile, JSON.stringify({gitaccess: 'https', token: 'abc', User: 'def' }, null, "  "), function (err) {
+        ];
+        //noinspection JSUnresolvedFunction
+        inquirer.prompt(questions).then(function (answers) {
+            answers.gitaccess = 'https';
+            fs.writeFile(conffile, JSON.stringify(answers, null, "  "), function (err) {
                 if (err) {
-                      console.log(colors.red('Cannot save config!'));
+                    console.log(colors.red('Cannot save config!'));
                     console.log(err);
                     process.exit(1);
                 }
                 console.log(colors.yellow('Note: Created config file at: ' + conffile));
                 console.log(colors.yellow('Note: Use cagr or clone-all-github-repos to clone all your repos into the current folder'));
+            });
+        });
+    } else {
+        fs.writeFile(conffile, JSON.stringify({ gitaccess: 'https', token: 'abc', User: 'def' }, null, "  "), function (err) {
+            if (err) {
+                console.log(colors.red('Cannot save config!'));
+                console.log(err);
+                process.exit(1);
+            }
+            console.log(colors.yellow('Note: Created config file at: ' + conffile));
+            console.log(colors.yellow('Note: Use cagr or clone-all-github-repos to clone all y our repos into the current folder'));
+        });
     }
 }
